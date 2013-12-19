c83ca5b8a06d364616ccd64d25bd40e9f6c19103
Andrew King <me@andrewsking.com>
2013-12-20 10:08:03 +1100

    Explicitly load koolhint in controller

--- app/controllers/kool_hints/kool_hints_controller.rb
+++ app/controllers/kool_hints/kool_hints_controller.rb
@@ -1,17 +1,23 @@
 module KoolHints
   class KoolHintsController < ApplicationController
-    load_and_authorize_resource
+    before_filter :load_kool_hint
 
     def edit
     end
 
     def update
-      if @hint.update_attributes(params[:hint])
+      if @koolhint.update_attributes(params[:hint])
         flash[:notice] = "Hint updated"
         redirect_to return_to_path
       else
         render :edit
       end
+    end
+
+    private
+
+    def load_kool_hint
+      @koolhint = KoolHint.find(params[:id])
     end
   end
 end
